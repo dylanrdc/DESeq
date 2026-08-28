df2ca1ec7a460dcccea10a40190c03a4a18ca816
U-PC002596\Dylan David <drdavid693@gmail.com>
2026-08-28 17:45:37 -0400

    trial

---
+++ runDESeq2.R
@@ -0,0 +1,31 @@
+
+# Step 1: preparing count data ----------------
+
+# read in counts data
+counts_data <- read.csv('counts_data.csv')
+head(counts_data)
+
+
+# read in sample info
+colData <- read.csv('sample_info.csv')
+
+
+# making sure the row names in colData matches to column names in counts_data
+all(colnames(counts_data) %in% rownames(colData))
+
+
+# jumble rows in colData - recreate the error
+colData <- colData[sample(1:nrow(colData)),]
+
+
+# are they in the same order?
+all(colnames(counts_data) == rownames(colData))
+counts_data <- counts_data[, rownames(colData)]
+
+
+# Step 2: construct a DESeqDataSet object ----------
+
+dds <- DESeqDataSetFromMatrix(countData = counts_data,
+                              colData = colData,
+                              design = ~ dexamethasone)
+
